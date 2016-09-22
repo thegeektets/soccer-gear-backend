abdba14ad2c63edfbf1c5a14f9a0c787c72be4de
Griffin Muteti <griffinmuteti31@gmail.com>
2016-09-22 16:44:34 +0300

    remove settings_private.py

--- soccer_gear/settings_private.py
+++
@@ -1,8 +0,0 @@
-env_variables = {}
-
-env_variables['DEBUG'] = True
-
-env_variables['DB_NAME'] = 'test'
-env_variables['DB_USER'] = 'postgres'
-env_variables['DB_PASSWORD'] = 'chyle1992'
-env_variables['DB_HOST'] = 'localhost'
