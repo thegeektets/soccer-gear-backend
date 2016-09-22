f4b387e24fc8969e59da295f0100893c985c4f6d
Stella Sikhila <stella@tunaweza.com>
2016-09-22 15:07:38 +0300

    setup authentication classes

--- soccer_gear/urls.py
+++ soccer_gear/urls.py
@@ -37,5 +37,6 @@ router.register(r'payments', PaymentViewSet, base_name='payments')
 
 urlpatterns = [
 	url(r'^api/v1/', include(router.urls)),
+	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
 ]
