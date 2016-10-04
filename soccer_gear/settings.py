34ca56c8a69eebd017056ba27aed6cb9a608e6c8
Mark Hatchell <mark.hatchell@prospect33.com>
2016-10-04 15:47:44 -0400

    added site app

--- soccer_gear/settings.py
+++ soccer_gear/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     #our apps
     'products',
@@ -54,6 +55,7 @@ INSTALLED_APPS = [
 
 
 ]
+SITE_ID = 1
 
 MIDDLEWARE_CLASSES = [
     'corsheaders.middleware.CorsMiddleware',
