5e7fc6e14b407220e5020f24ae1046ca00184649
Griffin Muteti <griffin@tunaweza.com>
2016-10-04 16:04:56 +0300

    autheticate product create,update,destroy

--- product/views_rest.py
+++ product/views_rest.py
@@ -23,6 +23,19 @@ class ProductViewSet(viewsets.ModelViewSet):
     filter_class = ProductFilter
     search_fields = ('title', 'price', )
 
+    def create(self, request, *args, **kwargs):
+        self.check_if_superuser(request)
+        self.set_user_on_data(request)
+        return super(ProductViewSet, self).create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        self.check_if_superuser(request)
+        self.set_user_on_data(request)
+        return super(ProductViewSet, self).update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        self.check_if_superuser(request)
+        return super(ProductViewSet, self).destroy(request, *args, **kwargs)
 
 class Product_CategoryViewSet(viewsets.ModelViewSet):
 
