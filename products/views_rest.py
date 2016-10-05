1abcae00d4691459403618e553f18a54e456aa21
Griffin Muteti <griffin@tunaweza.com>
2016-10-05 11:51:53 +0300

    create , delete , update checks

--- products/views_rest.py
+++ products/views_rest.py
@@ -25,12 +25,10 @@ class ProductViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         self.check_if_superuser(request)
-        self.set_user_on_data(request)
         return super(ProductViewSet, self).create(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
         self.check_if_superuser(request)
-        self.set_user_on_data(request)
         return super(ProductViewSet, self).update(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
