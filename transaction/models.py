1e3acc767c8798d71475b246da09f45cc0b1b9f5
Griffin Muteti <griffin@tunaweza.com>
2016-10-17 13:13:43 +0300

    Issue #27 payment is not required on order

--- transaction/models.py
+++ transaction/models.py
@@ -14,7 +14,7 @@ class Order(models.Model):
 
     user = models.ForeignKey('custom_auth.User', null=False, blank=False)
     status = models.CharField(null=False, blank=False, max_length=255)
-    payment = models.ForeignKey('transaction.Payment', null=True, blank=False)
+    payment = models.ForeignKey('transaction.Payment', null=True, blank=True)
     cost = models.CharField(null=False, blank=False, max_length=255)
     date_ordered = models.DateField(default= django.utils.timezone.now, null=False, blank=False, max_length=255)
 
