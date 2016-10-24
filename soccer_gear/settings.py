329aea8ab6b949f3f3abe10ca271339172b05f30
Griffin Muteti <griffin@tunaweza.com>
2016-10-24 17:35:11 +0300

    #27  updated setting.py

--- soccer_gear/settings.py
+++ soccer_gear/settings.py
@@ -182,8 +182,8 @@ REST_FRAMEWORK = {
 if DEBUG:
     LIPISHA_API_KEY = env_variables['LIPISHA_DEV_API_KEY']
     LIPISHA_API_SIGNATURE = env_variables['LIPISHA_DEV_API_SIGNATURE']
-    LIPISHA_API_ENV = env_variables['LIPISHA_DEV_ENV']
+    LIPISHA_API_ENV = 'test'
 else:
     LIPISHA_API_KEY = env_variables['LIPISHA_API_KEY']
     LIPISHA_API_SIGNATURE = env_variables['LIPISHA_API_SIGNATURE']
-    LIPISHA_API_ENV = env_variables['LIPISHA_API_ENV']
+    LIPISHA_API_ENV = 'live'
